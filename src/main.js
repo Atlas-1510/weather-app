c07ca848defd3cb87c85488d0a41842d68ca3fd7
Jason Aravanis <j.aravanis@icloud.com>
2021-04-12 17:28:29 +1000

    Add weather search for current and custom location
    * Add Nominatim API to get latitude and longitude based on user address
    input.
    
    * Add OpenWeatherMap API to get weather data based on latitude and
    longitude
    
    * Add basic form functionality to enable use of these APIs
    
    --Notes--
    
    Weather look up is based on geographic coordinates, rather than simple
    text lookup directly to OpenWeatherMap. This is to avoid the cases
    where a search for "Melbourne" was showing results for Melbourne, FL
    rather than Melbourne, Australia.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,58 @@
 import "./style.scss";
+
+const openweathermap_key = "8d3007697e1595ff555d6df24f4492f3";
+const location = document.getElementById("location");
+const locationSubmit = document.getElementById("locationSubmit");
+const geolocation = document.getElementById("geolocation");
+
+async function getCoordinatesAPI(input) {
+  // Uses nominatim API (https://nominatim.org) from OpenStreetMap to get latitude and longitude of user input
+  let API_response = await fetch(
+    `https://nominatim.openstreetmap.org/search?q=${input}&format=json&limit=1`,
+    {
+      mode: "cors",
+    }
+  );
+  return await parseResponse(API_response);
+}
+
+async function parseResponse(API_response) {
+  return await API_response.json();
+}
+
+async function getWeatherAPI(lat, lon) {
+  // Uses OpenWeatherMap one-call API (https://openweathermap.org/api/one-call-api) to get weather information
+  let API_response = await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?exclude=minutely&lat=${lat}&lon=${lon}&appid=${openweathermap_key}`,
+    { mode: "cors" }
+  );
+  return await parseResponse(API_response);
+}
+
+async function customWeatherSearch(searchInput) {
+  let locationJSON = (await getCoordinatesAPI(searchInput))[0];
+  console.log(locationJSON);
+  let weatherJSON = await getWeatherAPI(locationJSON.lat, locationJSON.lon);
+  console.log(weatherJSON);
+}
+
+async function currentLocationWeatherSearch() {
+  function getLocalCoordinates() {
+    return new Promise(function (resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+  }
+
+  const pos = await getLocalCoordinates();
+  const lat = pos.coords.latitude;
+  const lon = pos.coords.longitude;
+  let weatherJSON = await getWeatherAPI(lat, lon);
+  console.log(weatherJSON);
+}
+
+locationSubmit.addEventListener("click", (event) => {
+  event.preventDefault();
+  customWeatherSearch(location.value);
+});
+
+geolocation.addEventListener("click", currentLocationWeatherSearch);
---
+++ src/testCityList.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 2158177,
+    name: "Melbourne",
+    state: "",
+    country: "AU",
+    coord: {
+      lon: 144.963318,
+      lat: -37.813999,
+    },
+  },
+  {
+    id: 6173331,
+    name: "Vancouver",
+    state: "",
+    country: "CA",
+    coord: {
+      lon: -123.119339,
+      lat: 49.24966,
+    },
+  },
+];
